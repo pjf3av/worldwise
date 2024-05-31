cab13d37562bf04cffdbaf2348b7ceb9a0514fd9
Peter Farkas <pjf3av@gmail.com>
2024-05-31 16:41:03 -0400

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import AppNav from "./AppNav";
+import Footer from "./Footer";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+}
